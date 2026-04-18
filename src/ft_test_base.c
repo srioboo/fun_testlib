d918f3619d839006098bd5c7b5281697ceaa2626
Salvador Rioboo Naranjo <salrio74@gmail.com>
2026-04-18 09:14:41 +0200

    clean: clean norm errors

--- src/ft_test_base.c
+++ src/ft_test_base.c
@@ -6,7 +6,7 @@
 
 #include "funtestlib.h"
 
-void fun_group_start(const char *method_message)
+void	fun_group_start(const char *method_message)
 {
 	printf(COLOR_RESET "\n" COLOR_RESET);
 	printf(COLOR_BLUE_BKG "=======================");
@@ -19,12 +19,14 @@ void fun_group_start(const char *method_message)
 
 void	fun_start(const char *method_message)
 {
-	printf("\n" COLOR_BLUE "==== START %s ====" COLOR_RESET "\n", method_message);
+	printf("\n" COLOR_BLUE "==== START %s ====" COLOR_RESET "\n",
+		method_message);
 }
 
 void	fun_end(const char *method_message)
 {
-	printf("\n" COLOR_BLUE "==== END %s ====" COLOR_RESET "\n\n", method_message);
+	printf("\n" COLOR_BLUE "==== END %s ====" COLOR_RESET "\n\n",
+		method_message);
 }
 
 void	fun_test_header(int num)
